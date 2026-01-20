8d614bff709ff2876092977c081984fc85ea9b43
mwlt <michael.wlt@proton.me>
2026-01-20 18:56:04 +0100

    simplifiy light sensor

--- src/main.cpp
+++ src/main.cpp
@@ -54,11 +54,8 @@ void setup() {
   }
   
   // Initialize light sensor (LDR)
-  if (lightSensor.begin()) {
-    Serial.println(F("LDR light sensor initialized"));
-  } else {
-    Serial.println(F("LDR light sensor not found!"));
-  }
+  lightSensor.begin();
+  Serial.println(F("LDR light sensor initialized"));
   
   // Test all LEDs to verify they work
   Serial.println(F("Testing all LEDs..."));
